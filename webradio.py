7fa7c100fbe842bfffd3d3f0a0eda21de635206b
Hubert Denkmair <xor@chaostreff.in>
2013-06-18 22:00:53 +0200

    detektor.fm current songs

--- webradio.py
+++ webradio.py
@@ -1,20 +1,33 @@
+#!/usr/bin/python
 from flask import Flask, request, redirect, url_for, jsonify
+from threading import Lock
 import json
+import os
+import sys
 import subprocess
+import time
+import urllib
 
-config = json.load(open("config.json"))
+config = json.load(open(os.path.dirname(__file__) + "/config.json"))
 app = Flask(__name__, static_folder='static', static_url_path='')
+if not "mplayer_lock" in app.__dict__:
+	app.mplayer_lock = Lock()
+	app.mplayer = None
 
 def mplayer_play(url):
-	if (app.mplayer==None):
-		app.mplayer = subprocess.Popen(["mplayer", "-slave", "-quiet", url], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-	else:
+	with app.mplayer_lock:
 		try:
-			app.mplayer.stdin.write("loadfile %s \n" % (url))
-			app.mplayer.stdin.flush()
+			if (app.mplayer!=None):
+				print "stream already running, closing..."
+				app.mplayer.stdin.write("quit\n")
+				app.mplayer.stdin.flush()
+				
+			print "starting up mplayer..."
+			app.mplayer = subprocess.Popen(["mplayer", "-ao","alsa:device=hw=1.1", "-slave", "-quiet", url], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+			print "ok"
 		except:
+			print "error:", sys.exc_info()
 			app.mplayer = None
-			mplayer_play(url)
 
 
 @app.route("/stream/play", methods=["GET", "POST"])
@@ -26,6 +39,13 @@ def play():
 
 @app.route("/stream/list", methods=["GET"])
 def list():
+	streams = config["streams"]
+	data = json.load(urllib.urlopen("http://detektor.fm/music.json"))
+	streams["detector_musik"]["current"] = "%s - %s" % (data["artist"], data["title"])
+
+	data = json.load(urllib.urlopen("http://detektor.fm/word.json"))
+	streams["detector_wort"]["current"] = "%s - %s" % (data["artist"], data["title"])
+
 	return jsonify(config["streams"])
 
 @app.route("/")
@@ -33,6 +53,5 @@ def index():
 	return redirect(url_for("static", filename="index.html"))
 
 if __name__ == "__main__":
-	app.mplayer = None
-	#app.debug = True
+	app.debug = False
 	app.run(host="0.0.0.0", port=8001)
