08d218361b6dd889fd1bbfb6617327e6cd1862dc
Hubert Denkmair <xor@chaostreff.in>
2013-06-19 22:34:28 +0200

    added decoding of shoutcast stream titles

--- webradio.py
+++ webradio.py
@@ -7,13 +7,14 @@ import sys
 import subprocess
 import time
 import urllib
+import thread
+import re
 
 config = json.load(open(os.path.dirname(__file__) + "/config.json"))
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 def mplayer_play(url):
 	app.mplayer.stdin.write("loadfile \"%s\"\n" % (url))
-	mplayer_set_volume(mplayer_get_volume())
 
 def mplayer_set_volume(volume):
 	print "setting volume: %s" % (volume)
@@ -23,14 +24,17 @@ def mplayer_set_volume(volume):
 def mplayer_get_volume():
 	return app.mplayer.volume
 
+def mplayer_get_title():
+	return app.mplayer.title
 
 
-
+@app.route("/status")
+def status():
+	return jsonify({"volume": mplayer_get_volume(), "title": mplayer_get_title()})
 
 @app.route("/volume", methods=["GET", "POST"])
 def volume():
 	if request.method == "POST":
-		print request.json
 		mplayer_set_volume(request.json['volume'])
 	return jsonify({"volume": mplayer_get_volume()})		
 	
@@ -56,8 +60,30 @@ def list():
 def index():
 	return redirect(url_for("static", filename="index.html"))
 
+def mplayer_task(app):
+	app.mplayer = subprocess.Popen(["mplayer", "-ao","alsa:device=hw=1.1", "-slave", "-idle", "-softvol" , "-volume", "0"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	app.mplayer.volume = 25
+	app.mplayer.title = ""
+	while True:
+		line = app.mplayer.stdout.readline().decode("utf8").strip()
+		if line.startswith("Starting playback"):
+			mplayer_set_volume(mplayer_get_volume())
+
+		if line.startswith("ICY Info:"):
+			for meta in line[9:].split(";"):
+				meta = meta.strip();
+				if meta.startswith("StreamTitle='") and meta.endswith("'"):
+					title = meta[13:-1]
+					for regexp in config["stream_titles"]["blacklist"]:
+						if re.search(regexp, title): break
+					else:
+						for (search,replace) in config["stream_titles"]["replace"]:
+							title = re.sub(search, replace, title)
+						app.mplayer.title = title
+						print "found title: ", app.mplayer.title
+		
+
 if __name__ == "__main__":
 	app.debug = False
-	app.mplayer = subprocess.Popen(["mplayer", "-ao","alsa:device=hw=1.1", "-quiet", "-slave", "-idle", "-softvol" , "-volume", "0"], stdout=None, stdin=subprocess.PIPE)
-	app.mplayer.volume = 0;
+	app.mplayer_thread = thread.start_new_thread(mplayer_task, (app,))
 	app.run(host="0.0.0.0", port=8001)
